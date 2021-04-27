bd265e85dc2f595c2ff8d1daf54d59687b1f0c81
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2021-04-27 16:24:15 -0300

    Nova build

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,6 +1,61 @@
 import _defineProperty from '@babel/runtime/helpers/defineProperty';
+import _toConsumableArray from '@babel/runtime/helpers/toConsumableArray';
 import { useRef, useEffect } from 'react';
 
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+function validateCPF(cpf) {
+  if (cpf.length < 14) return false;
+  cpf = cpf.replace(/\D/g, "");
+  if (cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+  var res = true;
+  [9, 10].forEach(function (j) {
+    var soma = 0,
+        r;
+    cpf.split(/(?=)/).splice(0, j).forEach(function (e, i) {
+      soma += parseInt(e) * (j + 2 - (i + 1));
+    });
+    r = soma % 11;
+    r = r < 2 ? 0 : 11 - r;
+    if (r != cpf.substring(j, j + 1)) res = false;
+  });
+  return res;
+}
+function validatePhone(phone) {
+  var phoneRegex = new RegExp("^\\([0-9]{2}\\)(([0-9]{4}-[0-9]{4})|([0-9]{5}-[0-9]{4}))$");
+  return phoneRegex.test(phone);
+}
+function isFunction(fn) {
+  return typeof fn === "function";
+}
+
+var logical = /*#__PURE__*/Object.freeze({
+	__proto__: null,
+	validateEmail: validateEmail,
+	validateCPF: validateCPF,
+	validatePhone: validatePhone,
+	isFunction: isFunction
+});
+
+function getOS() {
+  try {
+    if (typeof navigator.product === "string" && navigator.product.toLowerCase().search("react") > -1) {
+      var lib = "react-native";
+      return require(lib).Platform.OS === "ios" ? "iOS" : "Android";
+    }
+
+    var userAgent = window.navigator.userAgent;
+    if (userAgent.toLowerCase().search("win") > -1) return "Windows";
+    if (userAgent.toLowerCase().search("x11") > -1) return "Unix";
+    if (userAgent.toLowerCase().search("mac") > -1) return "MacOS";
+    if (userAgent.toLowerCase().search("linux") > -1) return "Linux";
+    return userAgent;
+  } catch (e1) {
+    return "Electron";
+  }
+}
 function searchInto(string1, string2) {
   try {
     return string1.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().search(string2.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) > -1;
@@ -8,10 +63,73 @@ function searchInto(string1, string2) {
     return false;
   }
 }
+function clearString(text) {
+  var extra = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var ignore = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+  if (typeof text !== "string") text += "";
+  var newText = "";
+  var clearStringFromThese = [".", ",", "/", "\\", "|", "-", "_", "(", ")", " "].concat(_toConsumableArray(extra));
+
+  for (var i = 0; i < text.length; i++) {
+    if (!clearStringFromThese.includes(text[i].toUpperCase()) || ignore.includes(text[i].toUpperCase())) newText += text[i];
+  }
+
+  return newText;
+}
+function mask(text, mask) {
+  var extra = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+  var ignore = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
+  var newText = "";
+  text = clearString(text + "", extra, ignore);
+  var j = 0;
+
+  for (var i = 0; i < text.length; i++) {
+    if (i <= mask.length - 1) {
+      if (mask[j] !== "#") {
+        newText += mask[j];
+        j++;
+        newText += text[i];
+      } else {
+        newText += text[i];
+      }
+
+      j++;
+    }
+  }
+
+  return newText.slice(0, mask.length);
+}
 
 var general = /*#__PURE__*/Object.freeze({
 	__proto__: null,
-	searchInto: searchInto
+	getOS: getOS,
+	searchInto: searchInto,
+	clearString: clearString,
+	mask: mask
+});
+
+function elevation(value) {
+  var string = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+  var color = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "#000";
+
+  if (string) {
+    return "\n            elevation: ".concat(value, ";\n            shadow-color: ").concat(color, ";\n            shadow-opacity: ").concat(0.015 * value + 0.18, ";\n            shadow-radius: ").concat(0.54 * value, "px;\n            shadow-offset: {\n                height: ").concat(0.6 * value, "px;\n            };\n        ");
+  }
+
+  return {
+    elevation: value,
+    shadowColor: color,
+    shadowOpacity: 0.0015 * value + 0.18,
+    shadowRadius: 0.54 * value,
+    shadowOffset: {
+      height: 0.6 * value
+    }
+  };
+}
+
+var style = /*#__PURE__*/Object.freeze({
+	__proto__: null,
+	elevation: elevation
 });
 
 function usePrevious(value) {
@@ -31,6 +149,6 @@ function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (O
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-var utils = _objectSpread(_objectSpread({}, general), hooks);
+var utils = _objectSpread(_objectSpread(_objectSpread(_objectSpread({}, logical), general), hooks), style);
 
 export default utils;
--- dist/index.umd.js
+++ dist/index.umd.js
@@ -1,13 +1,68 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@babel/runtime/helpers/defineProperty'), require('react')) :
-	typeof define === 'function' && define.amd ? define(['@babel/runtime/helpers/defineProperty', 'react'], factory) :
-	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['@octaldev/react-router'] = factory(global._defineProperty, global.react));
-}(this, (function (_defineProperty, react) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@babel/runtime/helpers/defineProperty'), require('@babel/runtime/helpers/toConsumableArray'), require('react')) :
+	typeof define === 'function' && define.amd ? define(['@babel/runtime/helpers/defineProperty', '@babel/runtime/helpers/toConsumableArray', 'react'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['@octaldev/react-router'] = factory(global._defineProperty, global._toConsumableArray, global.react));
+}(this, (function (_defineProperty, _toConsumableArray, react) { 'use strict';
 
 	function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
 	var _defineProperty__default = /*#__PURE__*/_interopDefaultLegacy(_defineProperty);
+	var _toConsumableArray__default = /*#__PURE__*/_interopDefaultLegacy(_toConsumableArray);
 
+	function validateEmail(email) {
+	  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	  return re.test(String(email).toLowerCase());
+	}
+	function validateCPF(cpf) {
+	  if (cpf.length < 14) return false;
+	  cpf = cpf.replace(/\D/g, "");
+	  if (cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+	  var res = true;
+	  [9, 10].forEach(function (j) {
+	    var soma = 0,
+	        r;
+	    cpf.split(/(?=)/).splice(0, j).forEach(function (e, i) {
+	      soma += parseInt(e) * (j + 2 - (i + 1));
+	    });
+	    r = soma % 11;
+	    r = r < 2 ? 0 : 11 - r;
+	    if (r != cpf.substring(j, j + 1)) res = false;
+	  });
+	  return res;
+	}
+	function validatePhone(phone) {
+	  var phoneRegex = new RegExp("^\\([0-9]{2}\\)(([0-9]{4}-[0-9]{4})|([0-9]{5}-[0-9]{4}))$");
+	  return phoneRegex.test(phone);
+	}
+	function isFunction(fn) {
+	  return typeof fn === "function";
+	}
+
+	var logical = /*#__PURE__*/Object.freeze({
+		__proto__: null,
+		validateEmail: validateEmail,
+		validateCPF: validateCPF,
+		validatePhone: validatePhone,
+		isFunction: isFunction
+	});
+
+	function getOS() {
+	  try {
+	    if (typeof navigator.product === "string" && navigator.product.toLowerCase().search("react") > -1) {
+	      var lib = "react-native";
+	      return require(lib).Platform.OS === "ios" ? "iOS" : "Android";
+	    }
+
+	    var userAgent = window.navigator.userAgent;
+	    if (userAgent.toLowerCase().search("win") > -1) return "Windows";
+	    if (userAgent.toLowerCase().search("x11") > -1) return "Unix";
+	    if (userAgent.toLowerCase().search("mac") > -1) return "MacOS";
+	    if (userAgent.toLowerCase().search("linux") > -1) return "Linux";
+	    return userAgent;
+	  } catch (e1) {
+	    return "Electron";
+	  }
+	}
 	function searchInto(string1, string2) {
 	  try {
 	    return string1.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().search(string2.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) > -1;
@@ -15,10 +70,73 @@
 	    return false;
 	  }
 	}
+	function clearString(text) {
+	  var extra = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+	  var ignore = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+	  if (typeof text !== "string") text += "";
+	  var newText = "";
+	  var clearStringFromThese = [".", ",", "/", "\\", "|", "-", "_", "(", ")", " "].concat(_toConsumableArray__default['default'](extra));
+
+	  for (var i = 0; i < text.length; i++) {
+	    if (!clearStringFromThese.includes(text[i].toUpperCase()) || ignore.includes(text[i].toUpperCase())) newText += text[i];
+	  }
+
+	  return newText;
+	}
+	function mask(text, mask) {
+	  var extra = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+	  var ignore = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
+	  var newText = "";
+	  text = clearString(text + "", extra, ignore);
+	  var j = 0;
+
+	  for (var i = 0; i < text.length; i++) {
+	    if (i <= mask.length - 1) {
+	      if (mask[j] !== "#") {
+	        newText += mask[j];
+	        j++;
+	        newText += text[i];
+	      } else {
+	        newText += text[i];
+	      }
+
+	      j++;
+	    }
+	  }
+
+	  return newText.slice(0, mask.length);
+	}
 
 	var general = /*#__PURE__*/Object.freeze({
 		__proto__: null,
-		searchInto: searchInto
+		getOS: getOS,
+		searchInto: searchInto,
+		clearString: clearString,
+		mask: mask
+	});
+
+	function elevation(value) {
+	  var string = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+	  var color = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "#000";
+
+	  if (string) {
+	    return "\n            elevation: ".concat(value, ";\n            shadow-color: ").concat(color, ";\n            shadow-opacity: ").concat(0.015 * value + 0.18, ";\n            shadow-radius: ").concat(0.54 * value, "px;\n            shadow-offset: {\n                height: ").concat(0.6 * value, "px;\n            };\n        ");
+	  }
+
+	  return {
+	    elevation: value,
+	    shadowColor: color,
+	    shadowOpacity: 0.0015 * value + 0.18,
+	    shadowRadius: 0.54 * value,
+	    shadowOffset: {
+	      height: 0.6 * value
+	    }
+	  };
+	}
+
+	var style = /*#__PURE__*/Object.freeze({
+		__proto__: null,
+		elevation: elevation
 	});
 
 	function usePrevious(value) {
@@ -38,7 +156,7 @@
 
 	function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty__default['default'](target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-	var utils = _objectSpread(_objectSpread({}, general), hooks);
+	var utils = _objectSpread(_objectSpread(_objectSpread(_objectSpread({}, logical), general), hooks), style);
 
 	return utils;
 
